ede8f28e1639c37480b9e609df51f1ca315d1f80
andremartingo <andre.martingo@gmail.com>
2018-06-07 15:17:37 +0100

    create coredata migration policy

---
+++ Mooskine/Model/UpdateToAttributedStringsPolicy.swift
@@ -0,0 +1,28 @@
+//
+//  UpdateToAttributedStringsPolicy.swift
+//  Mooskine
+//
+//  Created by André Martingo on 07/06/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class UpdateToAttributedStringsPolicy: NSEntityMigrationPolicy {
+    
+    override func createDestinationInstances(forSource sInstance: NSManagedObject, in mapping: NSEntityMapping, manager: NSMigrationManager) throws {
+        // Call super
+        try super.createDestinationInstances(forSource: sInstance, in: mapping, manager: manager)
+        
+        // Get the (updated) destination Note instance we're modifying
+        guard let destination = manager.destinationInstances(forEntityMappingName: mapping.name, sourceInstances: [sInstance]).first else { return }
+        
+        // Use the (original) source Note instance, and instantiate a new
+        // NSAttributedString using the original string
+        if let text = sInstance.value(forKey: "text") as? String {
+            destination.setValue(NSAttributedString(string: text), forKey: "attributedText")
+        }
+    }
+}
